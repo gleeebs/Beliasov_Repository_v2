fe3bf27eb441f2d6d94eea58adc3fb780a104a93
Oleg <beliasovgiu@edu.vogu>
2023-10-30 14:23:51 +0300

    Merge branch 'lastname'
        # Conflicts:
        #	pd.c

--- pd.c
+++ pd.c
@@ -2,13 +2,13 @@
 #include <string.h>
 int main(int argc, char **argv)
 {
-<<<<<<< HEAD
+
  char name[255];
  printf("Enter your name: ");
  fgets(name, 255, stdin);
  printf("length = %d\n", strlen(name)); /* debug line */
  printf("Hello, %s!\n", name);
-=======
+
  char first[255], last[255];
  printf("Enter your first name: ");
  fgets(first, 255, stdin);
@@ -16,6 +16,6 @@ int main(int argc, char **argv)
  printf("Now enter your last name: ");
  gets(last); /* buffer overflow? what's that? */
  printf("Hello, %s %s!\n", first, last);
->>>>>>> 87c026ea8f3aa4d0e2106062519873428c81fcf7
+
  return 0;
 }
